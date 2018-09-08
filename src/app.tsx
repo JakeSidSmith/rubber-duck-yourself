65c82fdb387bc8acf6106266e7096c90ccb306e2
Jake 'Sid' Smith <jake@dabapps.com>
2018-09-08 14:37:08 +0100

    Handle user input

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,87 @@
+import { ENTER_KEY_CODE, GREETINGS } from '^/constants';
+import { pickRandom } from '^/utils';
 import React, { Component } from 'react';
 
-export default class App extends Component {
-  public render() {
-    return <p>Hello, World!</p>;
+interface AppState {
+  value: string;
+  lastMessages: string[];
+  nextMessage: string;
+  conversation: MessageProps[];
+}
+
+interface MessageProps {
+  text: string;
+  bot: boolean;
+}
+
+// tslint:disable-next-line:variable-name
+const Message = ({ text, bot }: MessageProps) => {
+  return (
+    <div>
+      <strong>{bot ? 'Ducky' : 'You'}</strong>
+      <p>{text}</p>
+    </div>
+  );
+};
+
+export default class App extends Component<{}, AppState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      lastMessages: [],
+      nextMessage: pickRandom(GREETINGS),
+      value: '',
+      conversation: [],
+    };
   }
+
+  public render() {
+    return (
+      <div className="container">
+        <div className="conversation">
+          {this.state.conversation.map(Message)}
+        </div>
+        <form onSubmit={this.onSubmit}>
+          <textarea
+            value={this.state.value}
+            onKeyDown={this.onKeyPress}
+            onChange={this.onChange}
+          />
+        </form>
+      </div>
+    );
+  }
+
+  private onSubmit = (event?: React.FormEvent) => {
+    if (event) {
+      event.preventDefault();
+    }
+
+    const [, ...lastMessages] = this.state.lastMessages;
+
+    this.setState({
+      lastMessages: [...lastMessages, this.state.value],
+      conversation: [
+        ...this.state.conversation,
+        {
+          bot: false,
+          text: this.state.value,
+        },
+      ],
+      value: '',
+    });
+  };
+
+  private onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+
+  private onKeyPress = (event: React.KeyboardEvent) => {
+    if (event.keyCode === ENTER_KEY_CODE && !event.shiftKey) {
+      this.onSubmit();
+    }
+  };
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+export const ENTER_KEY_CODE = 13;
+
 export const GREETINGS = [
   'Hello',
   'Hey',
