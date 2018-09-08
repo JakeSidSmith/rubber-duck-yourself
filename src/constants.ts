c742ba127e844bd16c10ec664db4698e43541604
Jake 'Sid' Smith <jake@dabapps.com>
2018-09-08 14:04:26 +0100

    Initial messages

---
+++ src/constants.ts
@@ -0,0 +1,29 @@
+export const GREETINGS = [
+  'Hello',
+  'Hey',
+  'Howdy',
+  'Sup?',
+  'Yo!',
+  'Good day',
+  "What's up?", // tslint:disable-line:quotemark
+  "How's it going?", // tslint:disable-line:quotemark
+];
+
+export const INITIAL_MESSAGES = ['Right', 'Alright', 'Uhuh', 'Mhm', 'Okay'];
+
+export const MESSAGES = INITIAL_MESSAGES.concat([
+  'Yeah',
+  'Continue',
+  'Sure',
+  'I see',
+  'And then',
+  'Got ya',
+  'I understand',
+  "I'm not sure I understand", // tslint:disable-line:quotemark
+  'Any more details?',
+  'What do you mean?',
+  'Hmmm',
+  'Do tell',
+  'Anything else?',
+  'What next?',
+]);
